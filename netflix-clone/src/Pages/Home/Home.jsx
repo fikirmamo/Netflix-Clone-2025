40a0fc65e622221e1403a7ad225f1484c46bda5c
Fikirte M Offiga <fikirmamo@gmail.com>
2025-02-10 02:09:01 -0500

    Banner

--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../Components/Hearder/Header'
 import Footer from '../../Components/Footer/Footer'
+import Banner from '../../Components/Banner/Banner'
 
 const Home = () => {
   return (
     <>
         <Header/>
+        <Banner/>
         <Footer/>
     </>
   )
--- netflix-clone/src/Utils/Requests.js
+++ netflix-clone/src/Utils/Requests.js
@@ -1,7 +1,9 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
+console.log(API_KEY);
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  // fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchNetflixOriginals: `/discover/tv?api_key=f4a1dd3fba8d7f03c466065fc603bfd3&with_networks=213`,
   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
@@ -12,6 +14,5 @@ const requests = {
 };
 
 export default requests;
-
 
 // consistent credential errors while i am trying to get information about the movie eg movie ID
